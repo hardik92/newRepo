39c0c0df2d9ef4e52a9551f79a6855bc59f549a8
hardik.parpelli <hardik.parpelli@servicenow.com>
2024-08-13 13:33:59 +0530

    new files

---
+++ MainClass.java
@@ -0,0 +1,5 @@
+public class MainClass{
+    public static void main(String args[]){
+        System.out.println("You are in main method. Thank you");
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+This is a readme file. This repo is used to testing prompts and results.
